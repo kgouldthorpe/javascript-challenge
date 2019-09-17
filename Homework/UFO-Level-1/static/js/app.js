adb2e46572fd176c4268cb212e4f9f3aea845572
Kendall <kendall.gouldthorpe@gmail.com>
2019-09-16 20:37:12 -0400

    update

--- Homework/UFO-Level-1/static/js/app.js
+++ Homework/UFO-Level-1/static/js/app.js
@@ -1,4 +1,31 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// create table
+function display() {
+    var table = index.createElement("table");
+    for (var i=0; i<tableData.length; i++) {
+        var row = table.insertRow();
+        for (var j=0; j<orderArray[i].length; j++) {
+            var cell = row.insertCell();
+            cell.appendChild(document.createTextNode(orderArray[i][j]));
+        }
+    }
+    return table;
+}
+
+// install packages
+npm install nearest-date;
+var nearest = require('nearest-date');
+
+// create function
+function filterSearch(date){
+    var target = new Date(date)
+    var filtered = nearest(tableData.datetime, target)
+    return tableData.date(filtered)
+};
+
+// call function
+var dateSearched = tableData.filter(filterSearch);
+console.log(dateSearched)
+
